89428e726c9eb8194a138e6fc971d0813bf4c44a
Elshodev <karaokeelshod@gmail.com>
2025-02-02 18:35:13 +0500

    remove  ProfileSuccess

--- src/views/Profile/view/ProfileAdresPage.tsx
+++ src/views/Profile/view/ProfileAdresPage.tsx
@@ -9,10 +9,10 @@ import { useNavigate } from "react-router-dom";
 import { APP_ROUTES } from "../../../router";
 
 const ProfileAdresPage = () => {
-    const {data} = adresList();
+  const { data } = adresList();
 
-    const navigate = useNavigate()
-    console.log(data, 'data')
+  const navigate = useNavigate();
+  console.log(data, "data");
 
   return (
     <>
@@ -24,25 +24,23 @@ const ProfileAdresPage = () => {
           <ProfileAdress />
         </div> */}
         <div className="grid lg:flex md:grid-cols-2 xl:gap-x-[14px] gap-x-[10px] gap-y-[16px] ">
-            {data?.map((item: any, ind: any) => (
-               <>
-                <div className="flex items-center border-none gap-x-[10px] bg-buttonBg border-[1px] w-max px-[15px] py-[15px] rounded-[12px]">
-                   <IoHomeOutline size={25}/>
-                    <p className="text-[18px]">{item.address}</p>
-                </div>
+          {data?.map((item: any, ind: any) => (
+            <>
+              <div className="flex items-center border-none gap-x-[10px] bg-buttonBg border-[1px] w-max px-[15px] py-[15px] rounded-[12px]">
+                <IoHomeOutline size={25} />
+                <p className="text-[18px]">{item.address}</p>
+              </div>
             </>
-            ))}
-            <Button
-            onClick={() => navigate(`${APP_ROUTES.PROFILE}/${APP_ROUTES.ADD_NEW_ADRESS}`) }
+          ))}
+          <Button
+            onClick={() =>
+              navigate(`${APP_ROUTES.PROFILE}/${APP_ROUTES.ADD_NEW_ADRESS}`)
+            }
             className="!bg-darkGreen  !text-white w-full md:h-[56px] h-[46px] rounded-[8px] text-[14px] md:text-[16px] font-[500]"
             type="default"
           >
             Добавить адресс
           </Button>
-        </div>
-        <div  className="flex flex-col xl:gap-[60px] gap-[40px] mt-[30px]">
-        {/* <ProfileButtons /> */}
-        <ProfileSuccess />
         </div>
       </div>
     </>
